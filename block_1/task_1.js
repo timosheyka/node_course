121fb27628a57e9b0b72c2c08ffda6510c49c99a
timosheyka <timosheyka@DESKTOP-N15SSSN>
2023-08-16 14:34:17 +0300

    solution

---
+++ block_1/task_1.js
@@ -0,0 +1,24 @@
+const translations = {  
+	en: {  
+	greet: "Hello",  
+	intro: "Welcome to our website"  
+},  
+	fr: {  
+	greet: "Bonjour",  
+	intro: "Bienvenue sur notre site web"  
+}  
+};  
+
+const language = "fr"; // Change to "en" for English  
+const greeting = "greet";  
+const introduction = "intro";  
+  
+function localize(strings, arg, obj = translations) {
+    return translations[language][arg];
+}
+
+const localizedGreeting = localize`${greeting}`;  
+const localizedIntroduction = localize`${introduction}`;  
+
+console.log(localizedGreeting); // Expected: "Bonjour" (for language "fr")  
+console.log(localizedIntroduction); // Expected: "Bienvenue sur notre site web" (for language "fr")
