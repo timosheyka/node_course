3b7cb0394d9f876f25298f6fec05b68c7e0e70ab
timosheyka <timosheyka@DESKTOP-N15SSSN>
2023-09-05 11:08:23 +0300

    solution3.0

--- zad_8/task_1.js
+++ zad_8/task_1.js
@@ -1,9 +1,24 @@
-const promiseAll = async (promise_array) => {
+const promiseAll_1 = async (promise_array) => {
   const result = [];
   for (const promise of promise_array) {
     result.push(await promise);
   }
   return result;
+}
+
+function promiseAll(promise_array) {
+  const results = [];
+  return new Promise((resolve, reject) => {
+    promise_array.forEach((promise, index) => {
+      promise
+        .then(result => {
+          results[index] = result;
+          if (results.length === promise_array.length) 
+          { resolve(results); }
+        })
+        .catch(error => { reject(error);});
+    });
+  });
 }
 
 const promises = [
