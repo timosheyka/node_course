8cf60d59b6496e56c4041b5608066185f716d335
timosheyka <kulltima820@gmail.com>
2023-10-05 17:20:35 +0200

    advanced solution

--- zad_12/task_1_v2.js
+++ zad_12/task_1_v2.js
@@ -1,17 +1,17 @@
 const jsonString = '{"name": "John", "age": 30, "city": "New York", "bool": false, "nullValue" : null}';
 
-function object() {
+function object(str) {
     const obj = {};
     const regexName = /\"\w+\"\s*:/g;
     let result = null;
-    while ((result = regexName.exec(jsonString)) !== null) {
+    while ((result = regexName.exec(str)) !== null) {
         const idx = regexName.lastIndex;
         const name = result[0].replace(/[^a-zA-Z]+/g, '');
         let val;
 
         const regexVal = /\"(.*?)\"|(\d+)|true|false|null/g;
         regexVal.lastIndex = idx;
-        const matchVal = regexVal.exec(jsonString);
+        const matchVal = regexVal.exec(str);
         
         if (matchVal) {
             if (matchVal[1] !== undefined) { val = matchVal[1]; }
@@ -19,7 +19,7 @@ function object() {
                 if (matchVal[2] !== undefined) { val = parseInt(matchVal[2]); }
                 else { val = matchVal[0] === 'true' ? true : matchVal[0] === 'false' ? false : null; }
             }
-        } else { throw new Error('Haven\'t found match'); }
+        } else { throw new Error('Haven\'t found match'); }        
         obj[name] = val;
         //console.log('idx:' + idx + ' name - ' + name + ': value - ' + val);
     }
@@ -28,11 +28,11 @@ function object() {
 
 const jsonStringArray = '["John", 30, "New York", true, null]';
 
-function array() {
+function array(str) {
     const arr = [];
     const regexVal = /\"(.*?)\"|(\d+)|true|false|null/g;
     let result = null;
-    while((result = regexVal.exec(jsonStringArray)) !== null) {
+    while((result = regexVal.exec(str)) !== null) {
         if (result) {
             if (result[1]) val = result[1];
             else
@@ -46,14 +46,11 @@ function array() {
 
 function distributor(str) {
     switch (str[0]) {
-        case '{': return object();
-        case '[': return array();
+        case '{': return object(str);
+        case '[': return array(str);
         default: throw new Error('First character is invalid');
     }
 }
 
-let result = distributor(jsonStringArray);
-console.log('array', result);
-
-result = distributor(jsonString);
-console.log('object', result);
+console.log(distributor(jsonStringArray));
+console.log(distributor(jsonString));
