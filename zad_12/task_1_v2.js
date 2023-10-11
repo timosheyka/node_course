cdf4cbe1c35e57b90b4a3fbde876049624ab1af1
timosheyka <kulltima820@gmail.com>
2023-10-11 13:03:21 +0200

    advanced solution

--- zad_12/task_1_v2.js
+++ zad_12/task_1_v2.js
@@ -1,56 +1,105 @@
-const jsonString = '{"name": "John", "age": 30, "city": "New York", "bool": false, "nullValue" : null}';
+/**
+ * Object  
+    * start/end - {}
+    * value - { "name": value }
+    * separators - ,
+ * Array
+    * start/end - []
+    * value - [ value ]
+    * separators - ,
+ * Value
+    * object - (recursion)
+    * array - (recursion)
+    * string - "text" 
+    * number - Number(value)
+    * boolean - true/false
+    * null - null
+*/
 
-function object(str) {
-    const obj = {};
-    const regexName = /\"\w+\"\s*:/g;
-    let result = null;
-    while ((result = regexName.exec(str)) !== null) {
-        const idx = regexName.lastIndex;
-        const name = result[0].replace(/[^a-zA-Z]+/g, '');
-        let val;
+function myJSONParse(JSONString) {
 
-        const regexVal = /\"(.*?)\"|(\d+)|true|false|null/g;
-        regexVal.lastIndex = idx;
+    function value(str, startIdx = 0) {
+        const regexVal = /\"(.*?)\"|(-?\d+(\.\d*)?)|true|false|null/g;
+        regexVal.lastIndex = startIdx;
         const matchVal = regexVal.exec(str);
-        
+        let val;
         if (matchVal) {
-            if (matchVal[1] !== undefined) { val = matchVal[1]; }
-            else {
-                if (matchVal[2] !== undefined) { val = parseInt(matchVal[2]); }
-                else { val = matchVal[0] === 'true' ? true : matchVal[0] === 'false' ? false : null; }
+            if (matchVal[1] !== undefined) val = matchVal[1];
+            else 
+                if (matchVal[2] !== undefined) val = parseFloat(matchVal[2]);
+                else val = matchVal[0] === 'true' ? true 
+                        : matchVal[0] === 'false' ? false : null;
+        } else {
+            throw new Error('Haven\'t found match');
+        }
+        return {"val" : val, "endIdx" : regexVal.lastIndex };
+    }
+
+    function cutNesting(str, startIdx, endIdx, open, close) {
+        let countBraces = 1;
+        while (countBraces > 0 && endIdx < str.length) {
+            if (str[endIdx] === open) countBraces++;
+            if (str[endIdx] === close) countBraces--;
+            endIdx++;
+        }
+        return {"nestedStr": str.slice(startIdx, endIdx), "endIdx": endIdx };
+    }
+
+    function array(str, startIdx = 1) {
+        let arr = [];
+        let idx = startIdx; let v;
+
+        do {
+            switch (str[idx]) {
+                case '{':
+                    const obj = object(str);
+                    arr.push(obj.obj); idx = obj.endIdx;
+                    break;
+                case '[':
+                    const a = array(str, idx + 1);
+                    arr.push(a.arr); idx = a.idx;
+                    break;
+                default:
+                    v = value(str, idx);
+                    arr.push(v.val);
+                    idx = v.endIdx + 1;
             }
-        } else { throw new Error('Haven\'t found match'); }        
-        obj[name] = val;
-        //console.log('idx:' + idx + ' name - ' + name + ': value - ' + val);
+        } while (idx !== str.length - 1);
+        return { "arr": arr, "idx": idx };
+    }    
+
+    function object(str) {
+        let obj = {};
+        str = str.replace(/\s/g, '');
+        const regexName = /\"(\w+)\"\s*:\s*/g;
+        let result = null, endIdx;
+        while ((result = regexName.exec(str))) {
+            const startIdx = regexName.lastIndex;
+            const name = result[1];
+            let val;
+            endIdx = startIdx + 1;
+            switch (str[startIdx]) {
+                case '{':
+                    const objSlice = cutNesting(str, startIdx, endIdx, '{', '}');
+                    val = object(objSlice.nestedStr).obj; endIdx = objSlice.endIdx;
+                    break;
+                case '[':
+                    const arrSlice = cutNesting(str, startIdx, endIdx, '[', ']');
+                    val = array(arrSlice.nestedStr).arr; endIdx = arrSlice.endIdx;
+                    break;
+                default:   
+                    const v = value(str, startIdx);
+                    val = v.val; endIdx = v.endIdx;      
+            }
+            obj[name] = val;
+            regexName.lastIndex = endIdx;
+        }
+        return {"obj": obj, "endIdx": endIdx };
     }
-    return obj;
+
+    return object(JSONString).obj;
 }
 
-const jsonStringArray = '["John", 30, "New York", true, null]';
-
-function array(str) {
-    const arr = [];
-    const regexVal = /\"(.*?)\"|(\d+)|true|false|null/g;
-    let result = null;
-    while((result = regexVal.exec(str)) !== null) {
-        if (result) {
-            if (result[1]) val = result[1];
-            else
-                if (result[2]) val = parseInt(result[2])
-                else { val = result[0] === 'true' ? true : result[0] === 'false' ? false : null; }
-        } else { throw new Error('Haven\'t found match'); }
-        arr.push(val);
-    }
-    return arr;
-}
-
-function distributor(str) {
-    switch (str[0]) {
-        case '{': return object(str);
-        case '[': return array(str);
-        default: throw new Error('First character is invalid');
-    }
-}
-
-console.log(distributor(jsonStringArray));
-console.log(distributor(jsonString));
+const testJSON = 
+'{"first": [ "1", 2, true, null , null, { "fsecond": "fsecond" }, [1, 2] ], "second": { "fthird": "fthird", "fsecond": "fsecond" }, "third": "third", "null": null}';
+console.log(myJSONParse(testJSON));
